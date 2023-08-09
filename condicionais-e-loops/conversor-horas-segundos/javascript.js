015e836d351ac67a2366a2a7e773b95783a705df
fabionascimentodev <fabio.fbn1@gmail.com>
2023-08-09 08:42:56 -0300

    first commit

--- condicionais-e-loops/conversor-horas-segundos/javascript.js
+++
@@ -1,9 +0,0 @@
-function horasParaSegundos(horas) {
-    return horas * 3600; // 1 hora = 3600 segundos
-}
-
-function conversor() {
-    let inputHoras = parseFloat(document.getElementById("inputHoras").value);
-    let resultado = horasParaSegundos(inputHoras);
-    document.getElementById("resultado").textContent = resultado + " segundos";
-}
